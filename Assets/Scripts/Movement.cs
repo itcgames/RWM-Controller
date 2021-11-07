af2288813b7aa573e5d3a3df2a25f18a9cba185b
allynn alvarico <allynnalvarico@LAPTOP-DQOB50AP.station>
2021-11-07 19:14:00 +0000

    added the movement script and assigned to the player/sprite

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float m_speed = 2;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
+        {
+            MoveLeft();
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+        {
+            MoveRight();
+        }
+        if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W))
+        {
+            MoveUp();
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S))
+        {
+            MoveDown();
+        }
+
+    }
+
+    public void MoveLeft()
+    {
+        transform.Translate(Vector2.left * Time.deltaTime * m_speed);
+    }
+
+    public void MoveRight()
+    {
+        transform.Translate(Vector2.right * Time.deltaTime * m_speed);
+    }
+
+    public void MoveUp()
+    {
+        transform.Translate(Vector2.up * Time.deltaTime * m_speed);
+    }
+
+    public void MoveDown()
+    {
+        transform.Translate(Vector2.down * Time.deltaTime * m_speed);
+    }
+}
---
+++ Assets/Tests/MovementTest.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class MovementTest
+    {
+
+        [SetUp]
+        public void Setup()
+        {
+           
+        }
+
+        //[UnityTest]
+        //public IEnumerator MoveLeft()
+        //{
+        //}
+
+        //[UnityTest]
+        //public IEnumerator MoveRight()
+        //{
+
+        //}
+    }
+}
